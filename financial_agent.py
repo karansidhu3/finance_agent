6a159a698d45e2715c0a6655967a149b371c8581
Karan Sidhu <karansidhu5550@gmail.com>
2025-12-14 21:11:40 -0800

    second agent added

--- financial_agent.py
+++ financial_agent.py
@@ -8,11 +8,25 @@ web_search_agent = Agent(
   name="WebSearchAgent",
   role="A financial agent that uses web search to gather information about financial markets and companies.",
   model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-  tools=[
-    DuckDuckGo(),
-    YFinanceTool()
-  ],
+  tools=[DuckDuckGo(),],
   instructions=["Always provide sources for web search results."],
+  show_tool_calls=True,
+  markdown=True,
+)
+
+finacial_agent = Agent(
+  name="FinancialAgent",
+  role="A financial agent that provides investment advice based on market data and trends.",
+  model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+  tools=[
+    YFinanceTool(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news=True),
+  ],
+  instructions=[
+    "Use web search to gather the latest news and trends about financial markets and companies.",
+    "Analyze market data using the YFinance tool to provide informed investment advice.",
+    "Always cite your sources when providing information from web searches.",
+    "Use tables to display data clearly."
+  ],
   show_tool_calls=True,
   markdown=True,
 )
